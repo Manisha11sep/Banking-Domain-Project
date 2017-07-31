205e09e6deccedc664c07021b1d22d932a843dd7
Manisha11sep <er.manisha11sep@gmail.com>
2017-07-31 16:52:12 -0700

    Banking project-> Verify login page

---
+++ Selenium_Junit/src/BankingDomain/Utility.java
@@ -0,0 +1,16 @@
+package BankingDomain;
+
+
+
+// Declare some common parameters for scripts
+
+public class Utility {
+
+public static final String Driver_path = "C:\\Users\\manisha\\selenium\\chromedriver_win32\\chromedriver.exe";
+	
+public static final String BASE_URL = "http://www.demo.guru99.com/";
+	public static final int Wait_time = 30;
+	public static final String user_name="mngr88830";
+	public static final String password="gazehYd";
+	
+}
