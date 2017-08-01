c6a77b76ffdaecc56f8cdc50afc32a7be8562414
Manisha11sep <er.manisha11sep@gmail.com>
2017-07-31 17:14:04 -0700

    login verification page

---
+++ Banking domain/src/BankingProject/Utility.java
@@ -0,0 +1,13 @@
+package BankingProject;
+
+public class Utility {
+
+	public static final String Driver_path = "C:\\Users\\manisha\\selenium\\chromedriver_win32\\chromedriver.exe";
+	
+	public static final String BASE_URL = "http://www.demo.guru99.com/";
+		public static final int Wait_time = 30;
+		public static final String user_name="mngr88830";
+		public static final String password="gazehYd";
+		
+	}
+
---
+++ Banking domain/src/BankingProject/loginVerification.java
@@ -0,0 +1,71 @@
+package BankingProject;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import BankingProject.Utility;
+
+public class loginVerification {
+
+	
+	static WebDriver driver;
+	private static String baseUrl;
+	
+	public static void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\manisha\\selenium\\chromedriver_win32\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+		baseUrl= Utility.BASE_URL;
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+
+	}
+
+	
+	public static void main(String[] args) {
+		// Code to test the login page//
+
+	setup();
+
+		
+		try {
+			
+			driver.get("http://www.demo.guru99.com/V4/");
+		
+			driver.findElement(By.name("uid")).sendKeys(Utility.user_name);
+			driver.findElement(By.name("password")).sendKeys(Utility.password);
+	
+			driver.findElement(By.name("btnLogin")).click();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//Code to verify the successful login and Title of homepage//
+		
+	String expected_title=driver.getTitle();
+	Assert.assertEquals(expected_title,"Guru99 Bank Manager HomePage" );
+	System.out.println("Title of home page is matched");
+	
+	String Welcome = driver.findElement(By.xpath("//marquee")).getText();
+if(Welcome.contains("Welcome"))
+		{
+	System.out.println("Test case passed. Login successfully");
+	
+}
+else
+{
+	System.out.println("Test case failed");
+}
+	
+		
+
+	}
+
+}
