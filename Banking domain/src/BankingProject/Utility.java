a87b73477cbc73f63e8148cde88eb82dc3941bdb
Manisha11sep <er.manisha11sep@gmail.com>
2017-08-03 23:07:21 -0700

    Created new function with parameters to verify login details from excel sheet

--- Banking domain/src/BankingProject/Utility.java
+++ Banking domain/src/BankingProject/Utility.java
@@ -1,13 +1,72 @@
 package BankingProject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class Utility {
 
 	public static final String Driver_path = "C:\\Users\\manisha\\selenium\\chromedriver_win32\\chromedriver.exe";
 	
-	public static final String BASE_URL = "http://www.demo.guru99.com/";
+	public static final String BASE_URL = "http://www.demo.guru99.com/V4/";
 		public static final int Wait_time = 30;
 		public static final String user_name="mngr88830";
 		public static final String password="gazehYd";
+		public static final String FILE_PATH="C:\\Users\\manisha\\workspace\\BankingDomain\\testData.xlsx";
+		public static final String SHEET_NAME = "Data";
+		public static final String TABLE_NAME="testData";
+		public static final String EXPECT_TITLE = "Guru99 Bank Manager HomePage";
+	    public static final String EXPECT_ERROR = "User or Password is not valid";
 		
-	}
+		
+
+public static String[][]  readingFile(String file_path) throws Exception
+{
+	String [][] data = new String[10][10];
+		
+			
+				//file_path= Utility.FILE_PATH;
+				File src = new File(file_path);
+				FileInputStream fis = new FileInputStream(src);
+				XSSFWorkbook wb=new XSSFWorkbook(fis);
+				XSSFSheet sheet1=wb.getSheetAt(0);
+				
+				int rowCount= sheet1.getLastRowNum()-sheet1.getFirstRowNum();
+				System.out.println("total number of rows are "+ rowCount);
+		
+				
+				for(int i=1; i<sheet1.getLastRowNum(); i++)
+				{
+			
+					XSSFRow row = sheet1.getRow(i);
+					for(int j=0; j<row.getLastCellNum(); j++)
+					{
+						//HSSFCell cell= row.getCell(j, row.CREATE_NULL_AS_BLANK); 
+						//String value = String.valueOf(cell);
+						String value = row.getCell(j).getStringCellValue();
+						data[i][j]=value;
+						System.out.println("The array values are " + data[i][j]);
+					}
+					
+				}
+			
+				
+		return data;
+			
+			
+}
+}
+
 
--- Banking domain/src/BankingProject/loginVerification.java
+++ Banking domain/src/BankingProject/loginVerification.java
@@ -1,9 +1,25 @@
 package BankingProject;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.poi.hslf.util.SystemTimeUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,7 +30,9 @@ public class loginVerification {
 
 	
 	static WebDriver driver;
-	private static String baseUrl;
+	 static String baseUrl;
+	public static String file_path;
+	String[][] data=null;
 	
 	public static void setup()
 	{
@@ -22,50 +40,118 @@ public class loginVerification {
 
 		driver = new ChromeDriver();
 		baseUrl= Utility.BASE_URL;
+		driver.get("http://www.demo.guru99.com/V4/");
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		//driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
 
 	}
 
-	
-	public static void main(String[] args) {
-		// Code to test the login page//
+	public static void loginVerify()
+	{
+		driver.get("http://www.demo.guru99.com/V4/");
+		driver.findElement(By.name("uid")).sendKeys(Utility.user_name);
+		driver.findElement(By.name("password")).sendKeys(Utility.password);
 
-	setup();
-
+		driver.findElement(By.name("btnLogin")).click();
+		String expected_title=driver.getTitle();
+		Assert.assertEquals(expected_title,"Guru99 Bank Manager HomePage" );
+		System.out.println("Title of home page is matched");
 		
-		try {
-			
-			driver.get("http://www.demo.guru99.com/V4/");
+		String Welcome = driver.findElement(By.xpath("//marquee")).getText();
+	if(Welcome.contains("Welcome"))
+			{
+		System.out.println("Test case passed. Login successfully");
 		
-			driver.findElement(By.name("uid")).sendKeys(Utility.user_name);
-			driver.findElement(By.name("password")).sendKeys(Utility.password);
-	
-			driver.findElement(By.name("btnLogin")).click();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//Code to verify the successful login and Title of homepage//
-		
-	String expected_title=driver.getTitle();
-	Assert.assertEquals(expected_title,"Guru99 Bank Manager HomePage" );
-	System.out.println("Title of home page is matched");
-	
-	String Welcome = driver.findElement(By.xpath("//marquee")).getText();
-if(Welcome.contains("Welcome"))
-		{
-	System.out.println("Test case passed. Login successfully");
-	
-}
-else
-{
-	System.out.println("Test case failed");
-}
-	
-		
-
 	}
+	else
+	{
+		System.out.println("Test case failed");
+	}
+		
+	}
+	
+	
+	
+	public static void loginVerifyFromExcel() 
+	{
+			try {
+				String[][] testData = Utility.readingFile(Utility.FILE_PATH);
+						
+				//driver.get(Utility.BASE_URL);
+					String username, password;
+					username = testData[1][0];
+					String username1 = testData[1][1];
+					
 
+					for(int i=1; i<= testData.length; i++)
+					{
+						username = testData[i][0];
+						password = testData[i][1];
+						System.out.println("User name is " + username);
+						System.out.println("Password is " + password);
+						setup();
+						
+						driver.findElement(By.name("uid")).sendKeys(username);
+						driver.findElement(By.name("password")).sendKeys(password);
+						driver.findElement(By.name("btnLogin")).click();
+						
+						String Welcome = driver.findElement(By.xpath("//marquee")).getText();
+						if(Welcome.contains("Welcome"))
+								{
+							System.out.println("Test case passed. Login successfully");
+							
+						}
+						else
+						{
+							System.out.println("Test case failed");
+						}
+					}
+					}
+						
+						/*Alert alt =driver.switchTo().alert();
+						String actualBoxtitle = alt.getText();
+						alt.accept();
+						if(actualBoxtitle.contains(Utility.EXPECT_ERROR))
+						{
+							System.out.println("Test case is Passed");
+							
+						}
+						else
+						{
+							System.out.println("Test case is failed");
+							
+						}
+						String actualTitle = driver.getTitle();
+						if(actualTitle == Utility.EXPECT_TITLE)
+						{
+						
+					System.out.println("Test case passed. Login successfully");
+						
+					}
+					else
+					{
+						System.out.println("Test case failed");
+					}
+					}
+			}
+			catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			 //driver.close();
+	}
+				*/
+				
+		
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public static void main(String[] args)  {
+		// Code to test the login page//
+		
+		loginVerifyFromExcel();
+
+}
 }
